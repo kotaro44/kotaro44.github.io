fa4233681c746993248a677c962fadbd320b54e0
carlos <carlos.sanchez@squadventure.com>
2018-03-25 02:15:41 +0800

    limiting results

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -84,20 +84,49 @@ Directives.directive('portfolio', ['Data', function portfolio(Data) {
     templateUrl: 'public/partials/portfolio.html',
     link: function portfolioLink(scope) {
       var allItems = JSON.parse(JSON.stringify(Data.portfolio));
+      var count = 0;
 
     	scope.portfolio = Data.portfolio;
       scope.columns = Data.columns;
+      scope.limit = true;
+
 
       scope.filters = ['All', 'Web Apps', 'Professional', 'Android', 'Social', 'Native Apps', 'Games', 'Education', 'Backend'];
       scope.selectedFilter = 'All';
 
       scope.applyFilter = function applyFilter(filter){
         scope.selectedFilter = filter;
+        scope.portfolio.map(function(item){
+          item.show = false;
+        })
       };
 
       scope.showItem = function showItem(item){
-        return scope.selectedFilter === 'All' || item.tags.indexOf(scope.selectedFilter) !== -1;
+        item.show = scope.selectedFilter === 'All' || item.tags.indexOf(scope.selectedFilter) !== -1
+        if (scope.limit && scope.portfolio.filter(function(a){return a.show}).length > 6) {
+          item.show = false;
+        }
+
+        return item.show;
       };
+
+      scope.removeLimit = function removeLimit() {
+        scope.limit = false;
+      };
+
+      scope.setLimit = function setLimit() {
+        scope.limit = true;
+      };
+
+      scope.isLimited = function isLimited(){
+        return scope.limit && scope.portfolio.filter(function(a){return a.show}).length >= 6;
+      };
+
+      scope.isNotLimited = function isNotLimited(){
+        return !scope.limit && scope.portfolio.filter(function(a){return a.show}).length >= 6;
+      };
+
+      
     },
     scope: {},
   };
